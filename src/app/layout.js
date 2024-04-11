826b3ca536c45e789156c9e8dc3816623f167b4e
jorgeochipinti97 <jorgeochipinti97@gmail.com>
2024-04-11 02:44:19 -0300

    @

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,22 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
-const inter = Inter({ subsets: ['latin'] })
-
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+const inter = Inter({ subsets: ["latin"] });
+import { Analytics } from "@vercel/analytics/react";
 export const metadata = {
-  title: 'Royer Store | Worldwide free shipping',
-  description: "Royer Store: Your global e-commerce hub for import and export. Discover a world of opportunities and seamless trade.",
-}
+  title: "Royer Store | Worldwide free shipping",
+  description:
+    "Royer Store: Your global e-commerce hub for import and export. Discover a world of opportunities and seamless trade.",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
-  )
+  );
 }
